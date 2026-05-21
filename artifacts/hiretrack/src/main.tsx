9b44d2eef3dc0217c3aaebb0d12a30650de95a48
Syed Afridh Pasha <afridsyed514@gmail.com>
2026-05-21 15:21:04 +0530

    fixed api url

--- artifacts/hiretrack/src/main.tsx
+++ artifacts/hiretrack/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "../../../lib/api-client-react/src/custom-fetch";
+
+setBaseUrl("https://hire-track-1.onrender.com");
+
 createRoot(document.getElementById("root")!).render(<App />);
